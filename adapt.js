79a73f410ec8f145ce6e1a553ce7d9a77dfa43fd
kaiete <kaiete@aragon-press.com>
2021-02-14 12:03:38 +0000

    Update adapt.js

--- adapt.js
+++ adapt.js
@@ -1,2 +1,12 @@
-var learn = {
-  
+const adapts = new Array()
+const adaptDefinitions = new Array()
+var adapt = {
+  add : function(adapt,def) {
+    adapts.push(adapt)
+    adaptDefinitions.push(def)
+  },
+  get : function(adaptToUse) {
+    let currentAdapt = adapts.indexOf(adaptToUse)
+    return adaptDefinitions[currentAdapt]
+  }
+}
